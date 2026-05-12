ad04368275c1737fada2ac0ebc10c8f4edd125e8
lbnets <161732324+lbnets@users.noreply.github.com>
2026-05-12 14:37:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "../app.jsx";
 import "../styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.React = React;
+globalThis.React = React;
+
+async function bootstrap() {
+  await import("../data.js");
+  await import("../components.jsx");
+
+  await import("../screen-login.jsx");
+  await import("../screen-dashboard.jsx");
+  await import("../screen-programs.jsx");
+  await import("../screen-participants.jsx");
+  await import("../screen-metrics.jsx");
+  await import("../screen-report.jsx");
+  await import("../screen-certificates.jsx");
+  await import("../screen-create.jsx");
+  await import("../screen-builder.jsx");
+  await import("../screen-learner.jsx");
+
+  await import("../app.jsx");
+
+  const App = window.App;
+
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+bootstrap();
